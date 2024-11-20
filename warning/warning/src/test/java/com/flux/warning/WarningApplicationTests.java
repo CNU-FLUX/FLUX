2f9250b9a3d8b8c63f49312c4affc6da5fc479f3
Hyeonseong0917 <sagiri0917@gmail.com>
2024-11-20 21:25:55 +0900

    Add Warning

---
+++ warning/warning/src/test/java/com/flux/warning/WarningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flux.warning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
