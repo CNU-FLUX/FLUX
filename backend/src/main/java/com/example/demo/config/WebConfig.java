f1fd0c533f08509284234f4061252e4dc6067b79
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-12-08 14:47:00 +0900

    Cors exposedHeaders

--- backend/src/main/java/com/example/demo/config/WebConfig.java
+++ backend/src/main/java/com/example/demo/config/WebConfig.java
@@ -12,6 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*") // 모든 출처 허용 (Credentials 포함 가능)
+                .allowedHeaders("*") // 모든 요청 헤더 허용
+                .exposedHeaders("Authorization", "Content-Type") // 노출할 응답 헤더
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowCredentials(true); // 쿠키 및 인증 정보 허용
     }
