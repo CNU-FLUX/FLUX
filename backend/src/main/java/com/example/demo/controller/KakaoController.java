081a7e32c18f46e746cb3fd019b908d6b3da7640
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-12-08 14:21:11 +0900

    @CrossOrigin 달아봄

--- backend/src/main/java/com/example/demo/controller/KakaoController.java
+++ backend/src/main/java/com/example/demo/controller/KakaoController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/kakao")
--- backend/src/main/java/com/example/demo/controller/NotificationController.java
+++ backend/src/main/java/com/example/demo/controller/NotificationController.java
@@ -7,12 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
